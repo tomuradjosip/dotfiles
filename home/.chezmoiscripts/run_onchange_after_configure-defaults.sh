dab3d069f77b73451e04a5bc21d5ed956edcb21a
Josip Tomurad <tomurad.josip@gmail.com>
2025-04-03 11:38:29 +0200

    Disable Spotlight shortcut, add 3D printing apps

--- home/.chezmoiscripts/run_onchange_after_configure-defaults.sh
+++ home/.chezmoiscripts/run_onchange_after_configure-defaults.sh
@@ -8,3 +8,9 @@ defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock orientation -string "left"
 
 killall Dock
+
+# Configure sleep
+sudo pmset displaysleep 60
+
+# Disable Spotlight shortcut
+defaults write com.apple.symbolichotkeys.plist AppleSymbolicHotKeys -dict-add 64 "{ enabled = 0; value = { parameters = (32, 49, 1048576); type = standard; }; }"
